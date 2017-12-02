3eb4229c4ddb45f8968bb839bf7b1d1542b1345f
Will Furnass <will@thearete.co.uk>
2017-12-02 12:26:45 +0000

    Add solution to Day 2 part 2

--- 2017/02.py
+++ 2017/02.py
@@ -1,37 +1,78 @@
-"""As you walk through the door, a glowing humanoid shape yells in your direction. "You there! Your state appears to be idle. Come help us repair the corruption in this spreadsheet - if we take another millisecond, we'll have to display an hourglass cursor!"
+"""
+
+Day 2 Part 1:
+    
+As you walk through the door, a glowing humanoid shape yells in your direction. "You there! Your state appears to be idle. Come help us repair the corruption in this spreadsheet - if we take another millisecond, we'll have to display an hourglass cursor!"
 
 The spreadsheet consists of rows of apparently-random numbers. To make sure the recovery process is on the right track, they need you to calculate the spreadsheet's checksum. For each row, determine the difference between the largest value and the smallest value; the checksum is the sum of all of these differences.
 
-For example, given the following spreadsheet:
 
-5 1 9 5
-7 5 3
-2 4 6 8
+What is the checksum for the spreadsheet in your puzzle input?
 
-    The first row's largest and smallest values are 9 and 1, and their difference is 8.
-    The second row's largest and smallest values are 7 and 3, and their difference is 4.
-    The third row's difference is 6.
+Day 2 Part 2:
 
-In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
+"Great work; looks like we're on the right track after all. Here's a star for your effort." However, the program seems a little worried. Can programs be worried?
 
-What is the checksum for the spreadsheet in your puzzle input?"""
+"Based on what we're seeing, it looks like all the User wanted is some information about the evenly divisible values in the spreadsheet. Unfortunately, none of us are equipped for that kind of calculation - most of us specialize in bitwise operations."
+
+It sounds like the goal is to find the only two numbers in each row where one evenly divides the other - that is, where the result of the division operation is a whole number. They would like you to find those numbers on each line, divide them, and add up each line's result.
+
+"""
 
 import os
+from itertools import chain, combinations
 from aoc import get_raw_input
 
 
-def test():
+def test_part_1():
     spreadsheet = [
         [5, 1, 9, 5],
         [7, 5, 3],
         [2, 4, 6, 8]
     ]
-    assert corruption_checksum(spreadsheet) == 18
+    """The first row's largest and smallest values are 9 and 1, and their difference is 8.
+    The second row's largest and smallest values are 7 and 3, and their difference is 4.
+    The third row's difference is 6.
+    In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18."""
+    assert corruption_checksum_part_1(spreadsheet) == 18
 
 
-def corruption_checksum(spreadsheet):
+def test_part_2():
+    spreadsheet = [
+        [5, 9, 2, 8],
+        [9, 4, 7, 3],
+        [3, 8, 6, 5],
+    ]
+    """In the first row, the only two numbers that evenly divide are 8 and 2; 
+    the result of this division is 4.
+    In the second row, the two numbers are 9 and 3; the result is 3.
+    In the third row, the result is 2.
+
+    In this example, the sum of the results would be 4 + 3 + 2 = 9."""
+    print(corruption_checksum_part_2(spreadsheet))
+    assert corruption_checksum_part_2(spreadsheet) == 9
+
+
+def corruption_checksum_part_1(spreadsheet):
     return sum(map(lambda row: max(row) - min(row),
                    spreadsheet))
+
+
+def _div_if_even_divisible(pair):
+    """Return result if division if evenly divisible"""
+    a, b = pair
+    return a / b if a % b == 0 else 0
+
+
+def _f(int_list):
+    """Sum of all divisions for all evenly divisible pairs in a list if ints"""
+    return sum(map(_div_if_even_divisible,
+                   combinations(sorted(int_list, reverse=True),
+                                2)))
+
+
+def corruption_checksum_part_2(spreadsheet):
+    return sum(map(_f, spreadsheet))
 
 
 if __name__ == '__main__':
@@ -42,4 +83,5 @@ if __name__ == '__main__':
                  filter(None,
                         txt.split(os.linesep)))
 
-    print(corruption_checksum(input_))
+    print(corruption_checksum_part_1(input_))
+    print(corruption_checksum_part_2(input_))
