351475edf4c05ce2d727150109b4b7446d1226db
Will Furnass <will@thearete.co.uk>
2017-12-09 10:13:40 +0000

    Add Python solution to day 4 part 2 and refactor part 1

--- 2017/04.py
+++ 2017/04.py
@@ -1,5 +1,10 @@
-""""
-Day 4 - part 1
+import os
+from functools import reduce
+
+from aoc import get_raw_input
+
+
+""""Day 4 - part 1
 
 A new system policy has been put in place that requires all accounts to use a
 passphrase instead of simply a password. A passphrase consists of a series of
@@ -9,12 +14,17 @@ To ensure security, a valid passphrase must contain no duplicate words.
 
 The system's full passphrase list is available as your puzzle input.
 How many passphrases are valid?
+
+Day 4 - part 2
+
+For added security, yet another system policy has been put in place. Now, a
+valid passphrase must contain no two words that are anagrams of each other -
+that is, a passphrase is invalid if any word's letters can be rearranged to
+form any other word in the passphrase.
+
+Under this new system policy, how many passphrases are valid?
+
 """
-
-import os
-from functools import reduce
-
-from aoc import get_raw_input
 
 
 def test_part_1():
@@ -28,15 +38,35 @@ def test_part_1():
     # as 'aa' and 'aaa' count as different words.
 
 
-def is_valid_pass(password):
+def test_part_2():
+
+    assert is_valid_pass('abcde fghij', part_2=True)
+
+    assert not is_valid_pass('abcde xyz ecdab', part_2=True)
+    # as the letters from the third word can be rearranged to form the first
+    # word.
+
+    assert is_valid_pass('a ab abc abd abf abj', part_2=True) 
+    # as all letters need to be used when forming another word.
+
+    assert is_valid_pass('iiii oiii ooii oooi oooo', part_2=True)
+
+    assert not is_valid_pass('oiii ioii iioi iiio', part_2=True)
+    # as any of these words can be rearranged to form any other word.
+
+
+def is_valid_pass(password, part_2=False):
     words = password.split()
+    if part_2:
+        words = [tuple(sorted(word)) for word in words]
     return len(words) == len(set(words))
 
 
 if __name__ == '__main__':
-    txt = get_raw_input()
-    cnt = reduce(lambda a, b: a + 1,
-                 filter(is_valid_pass,
-                        txt.split(os.linesep)),
-                 0)
-    print(cnt)
+    passwords = get_raw_input().split(os.linesep)
+
+    print("Part 1: {} passwords are valid".format(
+        sum(1 for pw in passwords if is_valid_pass(pw))))
+
+    print("Part 2: {} passwords are valid".format(
+        sum(1 for pw in passwords if is_valid_pass(pw, part_2=True))))
